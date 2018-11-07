5f1dcd81eb41bd6aa54f43c2b1398f2cb1cb2edc
Emanuel Tannert <post@etannert.de>
2018-11-07 20:12:16 +0100

    create php backend for vote counting + js lib (in the index.html file)

---
+++ stats.php
@@ -0,0 +1,28 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST' ||
+    !isset($_REQUEST['vote']) ||
+    count($_REQUEST) !== 1) {
+
+    header('HTTP/1.1 404 Not Found');
+    die;
+}
+
+$button_id = $_REQUEST['vote'];
+$stats_json = file_get_contents(__DIR__ . '/stats.json');
+$stats = json_decode($stats_json, true);
+
+if (!isset($stats[$button_id])) {
+    header('HTTP/1.1 404 Not Found');
+    die;
+}
+
+$stats[$button_id]++;
+$stats_json = json_encode($stats);
+$success = file_put_contents(__DIR__ . '/stats.json', $stats_json, LOCK_EX);
+if ($success === FALSE) {
+    header('HTTP/1.1 500 Internal Server Error');
+    die;
+}
+
+echo $stats[$button_id];
