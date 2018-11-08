2350d0998e5dee37fe945c7866409be35131a856
Emanuel Tannert <post@etannert.de>
2018-11-08 15:29:19 +0100

    don't double-vote (sorry)

--- scripts/buttons/jonas.js
+++ scripts/buttons/jonas.js
@@ -1,8 +1,5 @@
-import { voteFor } from '../stats'
-
 let button = document.querySelector(".jonas-button")
 console.log(button)
 button.addEventListener("click", () => {
-  voteFor('jonas-button')
   button.classList.toggle("geklickt")
 })
