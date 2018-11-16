81fb30dd07b387f98fa709362224498501bbd846
Emanuel Tannert <post@etannert.de>
2018-11-16 12:59:53 +0100

    fix the vote calls on alina's and tobija's buttons

--- scripts/buttons/alina.js
+++ scripts/buttons/alina.js
@@ -2,32 +2,32 @@ import { voteFor } from '../stats'
 
 let startImage = document.querySelector('.start-image')
 
-let animatedImage = document.querySelector(".animated-image")
+let animatedImage = document.querySelector('.animated-image')
 
-let cta = document.querySelector(".cta")
-let ctaLabel = document.querySelector(".cta-label")
+let cta = document.querySelector('.cta')
+let ctaLabel = document.querySelector('.cta-label')
 
-cta.addEventListener("click", toggle)
+cta.addEventListener('click', toggle)
 
 let isAnimationRunning = false
 
 function startAnimation() {
-  startImage.classList.remove("is-active")
-  animatedImage.classList.add("is-active")
-  ctaLabel.innerHTML = "STOP NOW"
+  startImage.classList.remove('is-active')
+  animatedImage.classList.add('is-active')
+  ctaLabel.innerHTML = 'STOP NOW'
   isAnimationRunning = true
 }
 
 function stopAnimation() {
-  startImage.classList.add("is-active")
-  animatedImage.classList.remove("is-active")
-  ctaLabel.innerHTML = "START NOW"
+  startImage.classList.add('is-active')
+  animatedImage.classList.remove('is-active')
+  ctaLabel.innerHTML = 'START NOW'
   isAnimationRunning = false
 }
 
 function toggle() {
-  voteFor('Alina')
-  if(isAnimationRunning) {
+  voteFor('alina')
+  if (isAnimationRunning) {
     stopAnimation()
   } else {
     startAnimation()
--- scripts/buttons/tobija.js
+++ scripts/buttons/tobija.js
@@ -3,41 +3,33 @@ let jQuery = $
 import { voteFor } from '../stats'
 
 function myFunction(e) {
-	var offsetInfo = jQuery("#tobijas-button").offset(),
-		x = e.clientX,
-		y = e.clientY,
-		clipHeight = $("#tobijas-button").height(),
-		clipWidth = $("#tobijas-button").width(),
-		elPositionX = Math.floor(x - offsetInfo.left),
-		elPositionY = Math.floor(y - offsetInfo.top),
-		percentageX = Math.floor((elPositionX / clipWidth) * 100),
-		percentageY = Math.floor((elPositionY / clipHeight) * 100);
+  var offsetInfo = jQuery('#tobijas-button').offset(),
+    y = e.clientY,
+    clipHeight = $('#tobijas-button').height(),
+    elPositionY = Math.floor(y - offsetInfo.top),
+    percentageY = Math.floor(elPositionY / clipHeight * 100)
 
-	$("#tobijas-button").css({
-    "background-image": "linear-gradient(180deg,#fff " + percentageY + "%,#999 " + elPositionY + "%)",
-    });
-}
-function eventFire(el, etype){
-  if (el.fireEvent) {
-    el.fireEvent('on' + etype);
-  } else {
-    var evObj = document.createEvent('Events');
-    evObj.initEvent(etype, true, false);
-    el.dispatchEvent(evObj);
-  }
+  $('#tobijas-button').css({
+    'background-image':
+      'linear-gradient(180deg,#fff ' +
+      percentageY +
+      '%,#999 ' +
+      elPositionY +
+      '%)',
+  })
 }
 function assignClass() {
-  $('#tobijas-button').addClass('tobijas-button-handler');
-  $('.tobijas-button-handler').on("click", function(){
-    voteFor('Tobija')
-    $(document.body).append('<div id="overlay"></div>');
+  $('#tobijas-button').addClass('tobijas-button-handler')
+  $('.tobijas-button-handler').on('click', function() {
+    voteFor('tobija')
+    $(document.body).append('<div id="overlay"></div>')
     $('#overlay').append('<div id="close-cross">+</div>')
-    $(document.body).addClass('body-blocked');
-      $('#close-cross').on("click", function(){
-        $('#overlay').remove();
-      });
-    });
+    $(document.body).addClass('body-blocked')
+    $('#close-cross').on('click', function() {
+      $('#overlay').remove()
+    })
+  })
 }
 
-$("#tobijas-button").mousemove(myFunction);
-assignClass();
+$('#tobijas-button').mousemove(myFunction)
+assignClass()
